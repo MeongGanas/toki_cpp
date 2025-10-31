ab58baecd10f88faa40d26838fa5af8f623d9acd
MeongGanas <farreluken@gmail.com>
2025-10-31 20:59:46 +0800

    latihan

---
+++ Satu_Dua_Tiga_Empat.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int X;
+    cin >> X;
+
+    if(X == 1){
+        cout << 1;
+    } else if (X == 2){
+        cout << 3;
+    } else if (X == 3){
+        cout << 6;
+    } else {
+        cout << 0;
+    }
+
+    return 0;
+}
